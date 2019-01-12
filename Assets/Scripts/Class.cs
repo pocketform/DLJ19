ea98385fe9d2423cb6bbaf51ec0d993ea70a8213
pocketMo <pocketform@gmail.com>
2019-01-12 11:54:35 +0800

    End of First Class
    learn Function, Array and variable

---
+++ Assets/Scripts/Class.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class : MonoBehaviour
+{
+    int a = 1;
+    float b = 3.1415926f;
+    string c = "hello";
+
+    int A;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("GET");
+
+        //Debug.Log(c);        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        A = ++a;
+        
+
+        if (A>5)
+        {
+            Debug.Log(A);
+        }
+        else
+        {
+            Debug.Log(" A < 5");
+        }
+    }
+}
